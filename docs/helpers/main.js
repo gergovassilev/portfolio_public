9707c0d661474107183988e02dd2f1bfe6604e7f
Gergo Vassilev <gergovassilev@gmail.com>
2026-07-19 20:54:54 +0300

    name changes

---
+++ docs/helpers/main.js
@@ -0,0 +1,20 @@
+/*
+Multyline comment for the file.
+
+This will be the main script with the common functions.
+
+*/
+
+
+
+
+// single line comment for the function
+async function loadPart(id, url) {
+  const html = await fetch(url).then(r => r.text());
+  document.getElementById(id).innerHTML = html;
+}
+
+
+
+loadPart("menu", "https://gergovassilev.github.io/portfolio_public/helpers/menu.html");
+loadPart("footer", "https://gergovassilev.github.io/portfolio_public/helpers/footer.html");
